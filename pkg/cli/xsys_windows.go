38a790b94f0b7ac426fbc58314a0f7c9ef6684fd
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-08-15 21:21:11 +0900

    fix: fix typo

--- pkg/cli/xsys_windows.go
+++ pkg/cli/xsys_windows.go
@@ -5,8 +5,8 @@ package cli
 
 import "errors"
 
-var errXSysNotSuppported = errors.New("Windows doesn't support AQUA_EXPERIMENTAL_X_SYS_EXEC")
+var errXSysNotSupported = errors.New("Windows doesn't support AQUA_EXPERIMENTAL_X_SYS_EXEC")
 
 func (runner *Runner) RunXSysExec(args ...string) error {
-	return errXSysNotSuppported
+	return errXSysNotSupported
 }
